ed5daedaaded0e54a2330f7936285292406b9f00
miau <2054052019hieu@ou.edu.vn>
2022-10-28 14:29:33 +0700

    Change Background

--- TinhToan/Form1.Designer.cs
+++ TinhToan/Form1.Designer.cs
@@ -113,6 +113,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.txtKetQua);
             this.Controls.Add(this.txtSoB);
