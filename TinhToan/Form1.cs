0ec4c29c0cef52f1788e42db8c5f75b3188b45ce
miau <2054052019hieu@ou.edu.vn>
2022-10-28 13:51:26 +0700

    Them DK cho bt Cong

--- TinhToan/Form1.cs
+++ TinhToan/Form1.cs
@@ -18,10 +18,13 @@ namespace TinhToan
         }
         private void btCong_Click(object sender, EventArgs e)
         {
-            double a = double.Parse(txtSoA.Text);
-            double b = double.Parse(txtSoB.Text);
-            double c = a + b;
-            txtKetQua.Text = c.ToString();
+            if (txtSoA.Text != String.Empty && txtSoB.Text != String.Empty)
+            {
+                double a = double.Parse(txtSoA.Text);
+                double b = double.Parse(txtSoB.Text);
+                double c = a + b;
+                txtKetQua.Text = c.ToString();
+            }
         }
 
         private void btTru_Click(object sender, EventArgs e)
